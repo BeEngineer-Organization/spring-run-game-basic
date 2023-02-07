6bcb19cb21dc31eac8f72ad00bbca7ad922d6154
guro-Ishiguro <ryu32349581@gmail.com>
2023-02-07 23:56:40 +0900

    プレイヤーのパスを削除

--- model_manager.py
+++ model_manager.py
@@ -13,7 +13,7 @@ PATH_TUNNEL_TEXTURE = "textures/tunnel.jpg"
 PATH_CAR = "models/car.glb"
 PATH_TUNNLE = "models/tunnel"
 # プレイヤーのパスを設定
-PATH_PLAYER = "models/player.glb"
+PATH_PLAYER = ""
 
 
 # プレイヤー
